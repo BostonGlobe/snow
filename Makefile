4a26fa9766d7f69d7407e2a72d562ab58931ce6a
Gabriel Florit <gabrielflorit@gmail.com>
2016-11-28 23:35:05 -0500

    Trying to get rid of < 0.001

--- Makefile
+++ Makefile
@@ -12,9 +12,15 @@ download:
 
 	curl 'http://www.nohrsc.noaa.gov/snowfall/data/${year}${month}/snfl_b2_${year}${month}${day}12_R150_L30_G0.20.tif' > input/snow.tif;
 
+preprocess:
+
+	gdal_calc.py -A input/snow.tif --outfile=output/snow.tif \
+		--calc="(A+1)*(A>=0.1)" --NoDataValue=0
+
 to_shapefile:
 
-	gdal_polygonize.py input/snow.tif -f "ESRI Shapefile" output/snow.shp;
+	gdal_polygonize.py output/snow.tif -f "ESRI Shapefile" output/snow.shp;
+	# python ~/Desktop/gdal_polygonize.py input/snow.tif -f "ESRI Shapefile" -p output/snow.shp;
 	ogr2ogr -where 'DN >= 0' -f "GeoJSON" output/snow.geojson output/snow.shp;
 
 to_topojson:
@@ -25,8 +31,27 @@ to_topojson:
 
 color:
 
-	gdaldem color-relief input/snow.tif color_ramp_default.txt output/output.tif -alpha;
+	gdaldem color-relief output/snow.tif color_ramp_default.txt output/output.tif -alpha;
 	convert output/output.tif output/output.png;
 
-all: clean_all download to_shapefile to_topojson color
+all: clean_all download preprocess to_shapefile to_topojson color
+
+test:
+	make clean dir=output
+	make preprocess
+	make to_shapefile
+	make to_topojson
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/js/main.js
+++ src/js/main.js
@@ -12,28 +12,27 @@ const svg = d3.select('svg.map')
 
 const feature = topojson.feature(snow, snow.objects.snow)
 const { features } = feature
-const projection = d3.geoMercator()
+const projection = d3.geoAlbers()
 const path = d3.geoPath().projection(projection)
 projection.fitSize([width, height], feature)
 
 const colors = [
-		[1000 * 0, '#ffffff'],
-		[1000 * 0.001, '#eaffbf'],
-		[1000 * 0.1, '#aaff7f'],
-		[1000 * 1, '#00cd00'],
-		[1000 * 2, '#008b00'],
-		[1000 * 3, '#104e8b'],
-		[1000 * 4, '#1e90ff'],
-		[1000 * 6, '#00b2ee'],
-		[1000 * 8, '#00eeee'],
-		[1000 * 10, '#c868ff'],
-		[1000 * 12, '#912cee'],
-		[1000 * 15, '#8b008b'],
-		[1000 * 18, '#8b0000'],
-		[1000 * 21, '#cd0000'],
-		[1000 * 24, '#ee4000'],
-		[1000 * 30, '#ff7f00'],
-		[1000 * 36, '#ffff00'],
+		[0, '#ffffff'],
+		[1 + 0, '#aaff7f'],
+		[1 + 1, '#00cd00'],
+		[1 + 2, '#008b00'],
+		[1 + 3, '#104e8b'],
+		[1 + 4, '#1e90ff'],
+		[1 + 6, '#00b2ee'],
+		[1 + 8, '#00eeee'],
+		[1 + 10, '#c868ff'],
+		[1 + 12, '#912cee'],
+		[1 + 15, '#8b008b'],
+		[1 + 18, '#8b0000'],
+		[1 + 21, '#cd0000'],
+		[1 + 24, '#ee4000'],
+		[1 + 30, '#ff7f00'],
+		[1 + 36, '#ffff00'],
 ]
 
 const color = d3.scaleThreshold()
@@ -45,10 +44,6 @@ svg.selectAll('path')
 	.enter().append('path')
 		.attr('d', path)
 		.attr('fill', d => color(d.properties.DN))
-
-// console.log(test)
-
-
 
 
 
