328d5452638b18a8b6ca6190e3d5d9055a262bc5
Gabriel Florit <gabrielflorit@gmail.com>
2016-12-13 22:59:57 -0500

    Add documentation

--- src/js/map.js
+++ src/js/map.js
@@ -4,13 +4,27 @@ const map = {
 
   init() {
 
+    // Select DOM map container
     const mapElement = select('.observed-snowfall--map')
 
+    // Create Mapzen map in map container
     const map = L.Mapzen.map(mapElement, {
     	center: [37.7749, -122.4194],
     	zoom: 13,
-    	scene: L.Mapzen.BasemapStyles.Cinnabar,
+    	scene: L.Mapzen.BasemapStyles.Refill,
     })
+
+    // Add geocoder
+    const geocoder = L.Mapzen.geocoder('mapzen-JA21Wes')
+    geocoder.setPosition('topright')
+    geocoder.addTo(map)
+
+    // Add locator button
+    const locator = L.Mapzen.locator()
+    locator.addTo(map)
+
+    // Keep track of map location in URL hash
+    L.Mapzen.hash({ map, geocoder })
 
   }
 
