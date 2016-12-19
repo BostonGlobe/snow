462236394384f8c237778c4dc1fcf069bab6f21e
Gabriel Florit <gabrielflorit@gmail.com>
2016-12-19 18:55:19 -0500

    Constrain zooming and bounds

--- src/js/map.js
+++ src/js/map.js
@@ -9,10 +9,15 @@ const map = {
 
 		// Create Mapzen map in map container
 		const map = L.Mapzen.map(mapElement, {
-			center: [37, -97],
+			center: [42.45, -73.089],
 			zoom: 3.75,
-			// scene: L.Mapzen.BasemapStyles.Refill,
 			scene: 'assets/scene.yaml',
+			minZoom: 2,
+			maxZoom: 10,
+			maxBounds: [
+				[23.40276490540795, -130.869140625], // southwest
+				[51.12421275782688, -58.00781249999999], // northeast
+			],
 		})
 
 		map.attributionControl.addAttribution('Snowfall analysis <a href="https://www.nohrsc.noaa.gov/snowfall/">NOHRSC</a>');
