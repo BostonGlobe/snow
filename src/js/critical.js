21f86d04b664c79c669ba2f2cd77376d1acd58bc
elaina <elaina.natario@gmail.com>
2016-12-20 14:43:23 -0500

    lots more basic setup and playground

--- src/js/critical.js
+++ src/js/critical.js
@@ -3,5 +3,6 @@ import loadFont from './utils/loadFont'
 loadFont([
 	{ family: 'Benton', suffix: 'bold' },
 	{ family: 'Miller', suffix: 'regular' },
+	{ family: 'Miller-Banner', suffix: 'italic' },
 	{ family: 'Miller-Banner', suffix: 'regular' }
 ])
