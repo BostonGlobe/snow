f0c2c5d6b0b5f2b0eef786cf1e70347da6d75b2c
elaina <elaina.natario@gmail.com>
2016-12-20 16:47:50 -0500

    bad css for prototyping

--- src/js/critical.js
+++ src/js/critical.js
@@ -3,6 +3,5 @@ import loadFont from './utils/loadFont'
 loadFont([
 	{ family: 'Benton', suffix: 'bold' },
 	{ family: 'Miller', suffix: 'regular' },
-	{ family: 'Miller-Banner', suffix: 'italic' },
 	{ family: 'Miller-Banner', suffix: 'regular' }
 ])
