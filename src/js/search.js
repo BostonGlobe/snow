53e90bc78351583b1f0539de25e6a014e71e432d
Gabriel Florit <gabrielflorit@gmail.com>
2016-12-26 13:49:46 -0500

    Limit search by bounds

--- src/js/search.js
+++ src/js/search.js
@@ -5,7 +5,7 @@ const mapzenKey = 'mapzen-PvGhJST'
 
 const search = (text, callback) => {
 
-	const url = `https://search.mapzen.com/v1/autocomplete?text=${text}&api_key=${mapzenKey}&boundary.country=USA&layers=locality`
+	const url = `https://search.mapzen.com/v1/autocomplete?text=${text}&api_key=${mapzenKey}&boundary.country=USA&layers=locality&boundary.rect.min_lon=-130.87&boundary.rect.min_lat=23.40&boundary.rect.max_lon=-58.01&boundary.rect.max_lat=51.12`
 
 	json(url, (error, data) => {
 
