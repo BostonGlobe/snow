8f6ce10248b3aa49931e8735027cde5ec3094126
Kevin Wall <kowall116@gmail.com>
2018-01-19 11:51:30 -0800

    Added comments to js

--- src/getGeoTiff.js
+++ src/getGeoTiff.js
@@ -2,6 +2,19 @@ import fetch from 'node-fetch'
 import moment from 'moment'
 import fs from 'fs'
 
+/**
+ * getGeoTiff
+ * datasource: https://www.nohrsc.noaa.gov/snowfall/
+ * NOHRSC uploads geotiffs of snowfall at certain times of day.
+ * We pull the previous 6 hours, 24 hours and season totals of snowfall
+ * by finding the nearest available upload of each geotiff.
+ * Then download each geotiff to a file with a filename corresponding
+ * to the data type (i.e. 6h, 24h, or total) and the date that the
+ * geotiff was uploaded.
+ */
+
+// File info for data source
+// TODO: make season total file dynamic
 const filePrefixes = [
   {
     prefix: 'sfav2_CONUS_6h_',
@@ -19,6 +32,13 @@ const filePrefixes = [
   }
 ]
 
+/**
+ * Takes a moment js date object and returns year, month, date, and hour
+ * in correct string format.
+ * @method getDate
+ * @param  {Object} datetime moment js date object
+ * @return {Object}
+ */
 const getDate = datetime => {
   return {
     year: datetime.format('YYYY'),
@@ -28,20 +48,54 @@ const getDate = datetime => {
   }
 }
 
+/**
+ * Function that returns the date-based datasource url as well as the
+ * corresponding date-based file stream in order to request the most recent data and
+ * download it into a file.
+ * @method getDateFiles
+ * @param  {Object}     datetime moment js date object
+ * @param  {String}     prefix   prefix of the filename that determines
+ *                               the date type (6hr, 24hr, season total)
+ * @param  {String}     filename prefix of destination filename (6h, 24h, ot total)
+ * @return {Object}              object with datasource url string and file stream
+ */
 const getDateFiles = (datetime, prefix, filename) => {
   const { year, month, date, hour } = getDate(datetime)
   const yearMonth = `${year}${month}`
+
+  // Datasource
   const url = `https://www.nohrsc.noaa.gov/snowfall/data/${yearMonth}/${prefix}${yearMonth}${date}${hour}.tif`
+  // Destination filename
   const file = fs.createWriteStream(`input/${filename}_${year}-${month}-${date}-${hour}.tiff`)
 
   return { url, file }
 }
 
+/**
+ * Function that returns the correct hour to request as data is only updated
+ * at certain hours of the day.
+ * @method getHour
+ * @param  {Number} hour          current utc hour of the day
+ * @param  {Number} interval      how often data is updated
+ * @return {Number}               the nearest hour where data is availble
+ */
 const getHour = (hour, interval = 12) => {
+  //
   const hours = [0,6,12,18].filter(time => ((time % interval) === 0) && hour >= time)
   return Math.max(...hours)
 }
 
+/**
+ * Fetches nearest avalable geotiff and downloads it to a file
+ * with a filename that contains the datatype (6h, 24h, and total)
+ * as well as the date and hour of when data was uploaded.
+ * @method fetchFile
+ * @param  {String}  prefix   prefix of the filename that determines
+ *                            the date type (6hr, 24hr, season total)
+ * @param  {Object}  utc      moment js date object
+ * @param  {Number}  interval how often data is updated
+ * @param  {String}  filename prefix of destination filename (6h, 24h, ot total)
+ */
 const fetchFile = (prefix, utc, interval, filename) => {
   let { url, file } = getDateFiles(utc, prefix, filename)
   console.log(`DOWNLOADING: ${url}`);
@@ -68,6 +122,7 @@ const fetchFile = (prefix, utc, interval, filename) => {
     })
 }
 
+// Loop through all the file objects and fetch and download
 filePrefixes.forEach(obj => {
   const { prefix, interval, filename } = obj
   const utc = moment.utc()
